a98953ffd00aa4342b1ecf4f6c589bbdc028c5e5
Mohammad Ghazanfar Ali Danish <ghazanfar.danish@iese.fraunhofer.de>
2023-08-18 10:43:54 +0200

    Refactors
    Signed-off-by: Mohammad Ghazanfar Ali Danish <ghazanfar.danish@iese.fraunhofer.de>

--- basyx.aasrepository/basyx.aasrepository-backend-mongodb/src/test/java/org/eclipse/digitaltwin/basyx/aasrepository/config/TestConfiguration.java
+++ basyx.aasrepository/basyx.aasrepository-backend-mongodb/src/test/java/org/eclipse/digitaltwin/basyx/aasrepository/config/TestConfiguration.java
@@ -11,12 +11,33 @@ import com.mongodb.client.MongoClient;
 @Configuration
 public class TestConfiguration {
 	
+	@Value("${spring.data.mongodb.host}")
+	private String host;
+	
+	@Value("${spring.data.mongodb.port}")
+	private int port;
+	
+	@Value("${spring.data.mongodb.username}")
+	private String userName;
+	
+	@Value("${spring.data.mongodb.password}")
+	private String password;
+	
 	@Value("${spring.data.mongodb.database}")
 	private String database;
 	
 	@Bean
 	public MongoDatabaseFactory databaseFactory(MongoClient client) {
+		StringBuilder connectionBuilder = new StringBuilder("mongodb://");
+		connectionBuilder.append(userName);
+		connectionBuilder.append(":");
+		connectionBuilder.append(password);
+		connectionBuilder.append("@");
+		connectionBuilder.append(host);
+		connectionBuilder.append(":");
+		connectionBuilder.append(port);		
 		return new SimpleMongoClientDatabaseFactory(client, database);
+//		return new SimpleMongoClientDatabaseFactory("mongodb://mongoAdmin:mongoPassword@localhost:27017/test");
 	}
 
 }
